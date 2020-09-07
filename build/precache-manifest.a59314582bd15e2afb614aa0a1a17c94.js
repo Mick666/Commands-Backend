949160ca0f2c83f3d813419a89243027405ee880
Michael <Mick666@hotmail.com.au>
2020-09-08 08:41:57 +1000

    Added front-end to backend as build

---
+++ build/precache-manifest.a59314582bd15e2afb614aa0a1a17c94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea647d21d8a49e249b75e86bd4930b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0688d3747df2caddcc",
+    "url": "/static/css/main.298da2fd.chunk.css"
+  },
+  {
+    "revision": "081af593ed21842833b9",
+    "url": "/static/js/2.ef5d20ca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ef5d20ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0688d3747df2caddcc",
+    "url": "/static/js/main.4cead1e4.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
